5448524eac745e20ba2f7eef55c3b913c42fe42a
Fahri Abdul Hakim <fahriabdhakim@yahoo.com>
2018-02-22 20:25:28 +0700

    Add sidebar, navbar, icon-feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react';
-import logo from './seventhLogo.png';
+import React, {Component} from 'react';
+
+import Navbar from './components/small/navbar'
+import Iconfeature from './components/small/icon-feature'
+import Sidebar from './components/medium/sidebar'
+import {Grid} from 'semantic-ui-react'
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-        </p>
-      </div>
+    return (<Router>
+      <Grid columns={1}>
+        <Grid.Column>
+        <Navbar/>
+        </Grid.Column>
+
+
+    <Grid.Row columns={1}>
+      <Grid.Column>
+        <Iconfeature/>
+      </Grid.Column>
+    </Grid.Row>
+  
+    <Grid.Row columns={2}>
+      <Grid.Column>
+        <Sidebar/>
+      </Grid.Column>
+
+      <Grid.Column>
+        <Sidebar/>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+
+    </Router>
     );
   }
 }
