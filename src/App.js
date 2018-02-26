6e0f76b1ba4788a1b940c1e835770c4ce8e26815
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-26 16:37:42 +0700

    Resolve conflicts homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import 'semantic-ui-css/semantic.min.css';
 import React, { Component } from 'react'
-import { Button, Dropdown, Menu,Grid, Image } from 'semantic-ui-react'
 import './App.css'
 import Homepage from './pages/homepage'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 class App extends Component {
     render() {
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,30 +2,19 @@ import React, { Component } from 'react';
 import Iconfeature from '../components/small/icon-feature'
 import Sidebar from '../components/medium/sidebar'
 import {Grid} from 'semantic-ui-react'
-import Headerhomepage from '../components/medium/header-homepage'
 import Footer from '../components/small/footer'
 import '../App.css';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom"
-
+import Navbar from '../components/small/navbar'
 import Kelaslist from '../components/medium/kelaslist'
 
 class Homepage extends Component {
     render() {
      return (
        <div className="App">
-         <header className="App-header">
-
-         </header>
-         <p className="App-intro">
-         </p>
-
-
        <Grid columns={1}>
          <Grid.Column>
          <Navbar/>
          </Grid.Column>
-
-
      <Grid.Row columns={1}>
        <Grid.Column>
          <Iconfeature/>
@@ -33,13 +22,18 @@ class Homepage extends Component {
      </Grid.Row>
 
      <Grid.Row columns={2}>
-       <Grid.Column>
+       <Grid.Column width={3}>
          <Sidebar/>
        </Grid.Column>
+        <Grid.Column width={13}>
+          <Kelaslist/>
+        </Grid.Column>
+     </Grid.Row>
 
-       <Grid.Column>
-         <Sidebar/>
-       </Grid.Column>
+     <Grid.Row>
+      <Grid.Column>
+        <Footer />
+      </Grid.Column>
      </Grid.Row>
    </Grid>
 </div>
