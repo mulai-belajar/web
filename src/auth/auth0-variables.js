f32c653db296234241fd125735676fbb64493f04
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-27 17:58:34 +0700

    Change callbackurl

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'mulaibelajar.auth0.com',
   clientId: 'Fk8GynHevgwu6sa1CFD4WDOgsuv6Veno',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://mulaibelajar.netlify.com/callback',
   audience: 'http://api.mulaibelajar.com'
 }
