c134a4987d2aba75e81fac2bbcf01ec3d70fa397
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-02-27 13:46:36 +0700

    Create FAQ page named faq.js in medium directory

--- src/App-dono.js
+++ src/App-dono.js
@@ -1,43 +1,33 @@
 import React, { Component } from 'react';
-import Iconfeature from './components/small/icon-feature'
-import Sidebar from './components/medium/sidebar'
 import {Grid} from 'semantic-ui-react'
-import Footer from './components/small/footer'
-import './App.css'
+
 import Navbar from './components/small/navbar'
-import Kelaslist from './components/medium/kelaslist'
+import FAQ from './components/medium/faq'
+import Footer from './components/small/footer'
+
+import './App.css'
+
 
 class Homepage extends Component {
     render() {
      return (
-       <div className="App">
+      <div className="App">
        <Grid columns={1}>
          <Grid.Column>
-         <Navbar/>
+           <Navbar/>
          </Grid.Column>
-     <Grid.Row columns={1}>
-       <Grid.Column>
-         <Iconfeature/>
-       </Grid.Column>
-     </Grid.Row>
-
-     <Grid.Row columns={2}>
-       <Grid.Column width={3}>
-         <Sidebar/>
-       </Grid.Column>
-        <Grid.Column width={13}>
-          <Kelaslist/>
-        </Grid.Column>
-     </Grid.Row>
-
-     <Grid.Row>
-      <Grid.Column>
-        <Footer />
-      </Grid.Column>
-     </Grid.Row>
-   </Grid>
-</div>
-
+        <Grid.Row columns={1}>
+         <Grid.Column>
+           <FAQ/>
+         </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+         <Grid.Column>
+          <Footer />
+         </Grid.Column>
+        </Grid.Row>
+       </Grid>
+      </div>
       );
     }
   }
