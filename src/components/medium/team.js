ca5a353f1c1fa53adadf1f43887ca63d4737b906
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-28 19:00:17 +0700

    Update grid padding for team

--- src/components/medium/team.js
+++ src/components/medium/team.js
@@ -3,9 +3,11 @@ import { Image, Grid, Icon, Card } from 'semantic-ui-react'
 
 const ImageExampleCircular = () => (
   <div>
+  <Grid centered columns={1} padded>
    <Grid.Column textAlign='center'>
      <h1 className='contact-header'>Tim Mulai Belajar</h1>
    </Grid.Column>
+  </Grid>
   <Grid columns={5} padded>
    <Grid.Column>
      <Card>
