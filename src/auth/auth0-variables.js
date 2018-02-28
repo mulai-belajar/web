21ca3860d419323123aa7f5e7c88ce148cb81bf4
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-28 19:39:06 +0700

    Normalize env variables with .env

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,9 @@
 export const AUTH_CONFIG = {
-  domain: 'mulaibelajar.auth0.com',
-  clientId: 'Fk8GynHevgwu6sa1CFD4WDOgsuv6Veno',
-  callbackUrl: 'http://mulaibelajar.netlify.com/callback',
-  audience: 'http://api.mulaibelajar.com'
+  domain: process.env.REACT_APP_AUTH_DOMAIN || "mulaibelajar.auth0.com",
+  clientId:
+    process.env.REACT_APP_AUTH_CLIENT_ID || "Fk8GynHevgwu6sa1CFD4WDOgsuv6Veno",
+  callbackUrl:
+    process.env.REACT_APP_AUTH_CALLBACK_URL ||
+    "http://mulaibelajar.netlify.com/callback",
+  audience: process.env.REACT_APP_AUTH_AUDIENCE || "http://api.mulaibelajar.com"
 }
