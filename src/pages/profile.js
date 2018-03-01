2993140b8664b176dd9b79b8aaf222473c86051c
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-03-01 14:36:25 +0700

    Add classes have been created by user

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
-import { Grid, Card, Image } from 'semantic-ui-react'
-import ListKelas from '../components/medium/list-kelas'
-import BoxOverview from '../components/medium/box-overview'
-import Table from '../components/medium/table-donasi'
+import { Grid, Card, Image, Button, Header, Modal, Divider } from 'semantic-ui-react'
+import axios from 'axios'
+import Auth from '../auth/auth'
+import Currency from 'react-currency-formatter'
+
+const auth = new Auth()
 
 export default class Profile extends Component {
   componentWillMount() {
-    this.setState({ profile: {} })
+    this.setState({
+      classes: [],
+      profile: {}
+    })
+
+    const { getAccessToken } = auth
+
+    const config = {
+      headers: {
+        'Authorization' : `Bearer ${getAccessToken()}`
+      }
+    }
+
     const { userProfile, getProfile } = this.props.auth
     if (!userProfile) {
       getProfile((err, profile) => {
         this.setState({ profile })
+
+        axios.get(`http://localhost:3333/api/class/user/${this.state.profile.nickname}`, config)
+          .then(response => this.setState({ classes: response.data.data }))
+          .catch(error => this.setState({ message: error.message }))
       })
     } else {
       this.setState({ profile: userProfile })
@@ -18,7 +36,7 @@ export default class Profile extends Component {
   }
 
   render() {
-    const { profile } = this.state
+    const { profile, classes } = this.state
     return(
       <Grid centered columns={1} padded>
         <Grid.Column width={4}>
@@ -34,10 +52,57 @@ export default class Profile extends Component {
             </Card.Content>
           </Card>
         </Grid.Column>
-        <Grid.Column width={12}>
-          <BoxOverview />
-          <ListKelas />
-          <Table />
+        <Grid.Column width={8}>
+          <Header size='large'>Daftar kelas yang telah dibuat</Header>
+        {classes.map(classes =>
+          <Card fluid>
+            <Card.Content>
+              <Grid columns={2} padded={true}>
+              <Grid.Column width={13}>
+                <Card.Header>
+                  <Header size='medium'>{classes.name}</Header>
+                  Dibuat pada : {classes.createdAt}
+                </Card.Header>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Modal className='modal-box' dimmer='blurring' trigger={<Button positive fluid>Detail</Button>}>
+                   <Modal.Header>{classes.name}</Modal.Header>
+                   <Modal.Content scrolling>
+                     <Modal.Description>
+                        <Grid columns={1} padded>
+                           <Grid.Column width={12}>
+                              {classes.description}
+                              <Divider />
+                              <Header size='tiny'>Target Donasi</Header>
+                                IDR&nbsp;
+                                <Currency
+                                  quantity={classes.total_donation}
+                                  pattern="##,### "
+                                  group="."
+                                />
+                              <Header size='tiny'>Donasi Terkumpul</Header>
+                                IDR&nbsp;
+                                <Currency
+                                  quantity={classes.now_donation}
+                                  pattern="##,### "
+                                  group="."
+                                />
+                           </Grid.Column>
+                           <Grid.Column width={4}>
+                              <Header size='small'>Donatur</Header>
+                              {classes.donatur.map(donatur =>
+                                <p>{donatur.donatur}</p>
+                              )}
+                           </Grid.Column>
+                        </Grid>
+                     </Modal.Description>
+                   </Modal.Content>
+                 </Modal>
+              </Grid.Column>
+              </Grid>
+            </Card.Content>
+          </Card>
+        )}
         </Grid.Column>
       </Grid>
     )
